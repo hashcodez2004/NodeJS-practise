74c516cf8390c378e2fe5e189036522475c954cb
hashcodez2004 <kumarsharmaharsh2004@gmail.com>
2024-06-10 00:15:08 +0530

    File handling in NodeJS

---
+++ file.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+//Sync
+// fs.writeFileSync("./test.txt", "Hello World");
+
+//Async
+// fs.writeFile("./test.txt", "Hello World!!",(err) => {});
+
+
+//synchronous task gives result in a variable but async will not work like that
+// const result = fs.readFileSync("./contacts.txt","utf-8");
+// console.log(result);
+
+
+
+//Async => does not return anything same for write file also we must always give a callback in this case
+// fs.readFile("./contacts.txt","utf-8",(err, result) => {
+//     if(err){
+//         console.log("Error",err);
+//     }
+//     else{
+//         console.log(result);
+//     }
+// });
+
+//in this case data will be added again and again not overwritten as in the case of write file
+// const now = new Date();
+// fs.appendFileSync("./test.txt",now.toLocaleTimeString());
+
+// fs.cpSync("./test.txt","./copy.txt"); //copies test to copy file
+
+// fs.unlinkSync("./copy.txt"); //deletes the file
+
+// console.log(fs.statSync("./test.txt")); //check the statistics
+
+// fs.mkdirSync("my-docs"); //creates new directory
+// fs.rmdirSync("my-docs"); //deletes any empty directory...But for non-empty directories use same with recursive option set to true

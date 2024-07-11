40623966baff264cf55a4e72083707bec32d2326
hashcodez2004 <kumarsharmaharsh2004@gmail.com>
2024-07-11 16:24:52 +0530

    starting Express and NodeJS

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,19 +1,22 @@
-const { log } = require("console");
 const http = require("http");
-const fs = require("fs");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}: ${req.url} New Req Rec\n`;
-    fs.appendFile('log.txt', log, (err, data) => {
-        switch(req.url){
-            case "/": res.end("HomePage");
-            break;
-            case "/about": res.end("I am Harsh Sharma");
-            break;
-            default: res.end("404 Error");
-        }
-        // res.end("Hello From Server");
-    });
+const app = express();
+
+app.get('/', (req,res)=>{
+    return res.send("Hello From About Page")
 });
+
+app.get('/about', (req,res)=>{
+    return res.send(
+        "Hello From About Page "+
+        "hey "+
+        req.query.name+
+        " you are "+
+        req.query.age
+    );
+})
+
+const myServer = http.createServer(app);
 
 myServer.listen(8000,() => console.log("Server Started!"));
